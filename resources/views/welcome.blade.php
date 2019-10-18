7f56b8d31ececfd33d179d690f4df19a4e2c2a87
oracel <zhunjiechen@gmail.com>
2019-10-18 19:51:15 +0200

    test git

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    GestFit
+                    GestFit test update
                 </div>
 
                 <div class="links">
