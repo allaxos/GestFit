f91357fe006f666069778697df2482cd8c506afc
SAAD <saad-99@hotmail.fr>
2019-10-22 18:08:37 +0200

    FiX beug bootstrap & add FR package

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-require('./bootstrap');
+window._ = require('lodash');
+window.Popper = require('popper.js').default;
+try {
+    window.$ = window.jQuery = require('jquery');
+    require('bootstrap');
+} catch (e) {}
