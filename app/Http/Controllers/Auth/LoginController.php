93e6fe8bfacc1bd3883a32898fe3642a05b7abfe
SAAD <saad-99@hotmail.fr>
2019-10-28 18:07:21 +0100

    update Verification validate mail

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/validationMail';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
