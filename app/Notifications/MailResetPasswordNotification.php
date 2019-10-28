1c68e1c7637f7ed39bf19aeaaef62a8c09787586
SAAD <saad-99@hotmail.fr>
2019-10-28 23:04:44 +0100

    Change notification Reset to french

--- app/Notifications/MailResetPasswordNotification.php
+++ app/Notifications/MailResetPasswordNotification.php
@@ -47,7 +47,7 @@ class MailResetPasswordNotification extends Notification
             ->greeting('Bonjour!')
             ->line('Vous recevez cet email car nous avons reçu une demande de réinitialisation du mot de passe pour votre compte.')
             ->action('réinitialisation ', url(config('http://127.0.0.1:8000').route('password.reset',$this->token,false)))
-            ->line('Ce lien de réinitialisation de mot de passe expirera dans 60 minutes.')
+            ->line('Ce lien de réinitialisation de mot de passe expirera dans 24 heurs .')
             ->line('Si vous n\'avez pas demandé de réinitialisation de mot de passe, aucune action supplémentaire n\'est requise.')
             ->line('Cordialement,')
             ->salutation('Équipes de GesFit');
