dd98a45249d1e8a1d4720bdf535870010085fa13
SAAD <saad-99@hotmail.fr>
2019-11-24 17:17:16 +0100

    up

--- app/Annonce.php
+++ app/Annonce.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Annonce extends Model
 {
-    //
+
+
+    protected $fillable=[
+        'name' ,
+        'prix',
+        'salle_id',
+        'dateLocation' ,
+        'timeDebut' ,
+        'timeFin' ,
+        'description',
+        'user_id' ,
+        ];
+
+    public function user(){
+
+        return $this->belongsTo(User::class);
+    }
+
+    public function salle(){
+
+        return $this->belongsTo(Salle::class);
+    }
 }
