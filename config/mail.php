6d06340a00af48a77a9a5a7794b42e681c8acdaf
SAAD <saad-99@hotmail.fr>
2019-12-01 16:35:11 +0100

    Guest annonces

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'contact@gesfit.be'),
+        'address' => env('MAIL_FROM_ADDRESS', 'support@gesfit.be'),
         'name' => env('MAIL_FROM_NAME', 'GesFit'),
     ],
 
