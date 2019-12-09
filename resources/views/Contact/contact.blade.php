4b93ae377ceb9c45a6cb0d56dedbfd16ce2e1572
SAAD <saad-99@hotmail.fr>
2019-12-09 18:44:56 +0100

    up

--- resources/views/Contact/contact.blade.php
+++ resources/views/Contact/contact.blade.php
@@ -3,31 +3,35 @@
 @section('content')
     <br>
     <div class="container">
-        <div class="row card ">
-            <h4  class="card-header bg-success btn-lg btn-block font-weight-bold" style="color: white;font-size: 200%"">Contactez-Nous</h4>
-            <div class="card-body">
-                <form action="{{ url('contact') }}" method="POST">
-                    @csrf
-                    <div class="form-group">
-                        <input type="text" class="form-control  @error('name') is-invalid @enderror" name="name" id="nom" placeholder="Votre nom" value="{{ old('nom') }}">
-                        @error('name')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <h4  class="card-header bg-dark btn-lg btn-block font-weight-bold" style="color: white;font-size: 200%">Contactez-Nous</h4>
+                    <div class="card-body">
+                        <form action="{{ url('contact') }}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <input type="text" class="form-control  @error('name') is-invalid @enderror" name="name" id="nom" placeholder="Votre nom" value="{{ old('nom') }}">
+                                @error('name')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control  @error('email') is-invalid @enderror" name="email" id="email" placeholder="Votre email" value="{{ old('email') }}">
+                                @error('email')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <textarea class="form-control  @error('message') is-invalid @enderror" name="message" id="message" placeholder="Votre message">{{ old('message') }}</textarea>
+                                @error('message')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <button type="submit" class="btn btn-outline-dark font-weight-bold">Envoyer !</button>
+                        </form>
                     </div>
-                    <div class="form-group">
-                        <input type="email" class="form-control  @error('email') is-invalid @enderror" name="email" id="email" placeholder="Votre email" value="{{ old('email') }}">
-                        @error('email')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <textarea class="form-control  @error('message') is-invalid @enderror" name="message" id="message" placeholder="Votre message">{{ old('message') }}</textarea>
-                        @error('message')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <button type="submit" class="btn btn-success font-weight-bold">Envoyer !</button>
-                </form>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/Utilisateur/indexUtilisateur.blade.php
+++ resources/views/Utilisateur/indexUtilisateur.blade.php
@@ -5,12 +5,12 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header bg-success btn-lg btn-block font-weight-bold" style="color: white;font-size: 200%"><i class="fas fa-chalkboard-teacher"></i> Tableau de bord</div>
+                    <div class="card-header bg-dark btn-dark btn-block font-weight-bold" style="font-size: 200%"><i class="fas fa-chalkboard-teacher"></i> Tableau de bord</div>
                     <div class="card-body">
-                        <div class=”panel-heading” style="margin: 1% ;"><a href="{{route('profilIndex')}}" class="btn btn-outline-success btn-lg btn-block font-weight-bold" style="font-size: 200%;"><i class="far fa-address-card"></i> Mon profit </a></div>
-                        <div class=”panel-heading” style="margin: 1%;"><a href="{{route('messagerieIndex')}}" class="btn btn-outline-success btn-lg btn-block font-weight-bold" style="font-size: 200%;"><i class="far fa-envelope"></i> Messagerie ({{$countMessageNotRead}})</a></div>
-                        <div class=”panel-heading” style="margin: 1%;"><a href="{{route('recherche')}}" class="btn btn-outline-success btn-lg btn-block font-weight-bold" style="font-size: 200%;"><i class="fas fa-search"></i> Rechercher </a></div>
-                        <div class=”panel-heading” style="margin: 1%;"><a href="#" class="btn btn-outline-success btn-lg btn-block font-weight-bold" style="font-size: 200%;"><i class="fas fa-hand-peace"></i> Mes reservation</a></div>
+                        <div class=”panel-heading” style="margin: 1% ;"><a href="{{route('profilIndex')}}" class="btn btn-outline-dark btn-lg btn-block font-weight-bold" style="font-size: 150%;"><i class="far fa-address-card"></i> Mon profit </a></div>
+                        <div class=”panel-heading” style="margin: 1%;"><a href="{{route('messagerieIndex')}}" class="btn btn-outline-dark btn-lg btn-block font-weight-bold" style="font-size: 150%;"><i class="far fa-envelope"></i> Messagerie ({{$countMessageNotRead}})</a></div>
+                        <div class=”panel-heading” style="margin: 1%;"><a href="{{route('recherche')}}" class="btn btn-outline-dark btn-lg btn-block font-weight-bold" style="font-size: 150%;"><i class="fas fa-search"></i> Rechercher </a></div>
+                        <div class=”panel-heading” style="margin: 1%;"><a href="#" class="btn btn-outline-dark btn-lg btn-block font-weight-bold" style="font-size: 150%;"><i class="fas fa-hand-peace"></i> Mes reservation</a></div>
                     </div>
                 </div>
             </div>
